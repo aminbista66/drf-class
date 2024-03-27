389bcd2dc69a11c918752ce51633456998b4125c
Amin Bista <aminbista6666@gmail.com>
2024-03-27 17:32:43 +0545

    drf: UserListSerializer

---
+++ user/seralizers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import User    
+
+# class UserSerializer(serializers.Serializer):
+#     name = serializers.CharField(required=False)
+#     email = serializers.EmailField()
+#     password = serializers.CharField()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['name', 'email', 'created_at']
